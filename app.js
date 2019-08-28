99bcee7d2a945acbfecb849d93b21f9dba030b5a
Dan Moffat <danmoffat@gmail.com>
2019-08-28 22:46:05 +0100

    Vid 3 16:10. Basic graphql initialisation setup. Let's take this upstairs!

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(
+  '/api',
+  graphQlHttp({
+    schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {}
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
